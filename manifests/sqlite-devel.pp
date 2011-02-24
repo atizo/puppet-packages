6a40c7b4b98da3e351d236a160c9a6b3edd071f1
Simon Josi <simon.josi@atizo.com>
2011-02-24 10:08:55 +0100

    add sqlite

---
+++ manifests/sqlite-devel.pp
@@ -0,0 +1,5 @@
+class packages::sqlite-devel {
+  package{'sqlite-devel':
+    ensure => present,
+  }
+}
---
+++ manifests/sqlite.pp
@@ -0,0 +1,5 @@
+class packages::sqlite {
+  package{'sqlite':
+    ensure => present,
+  }
+}
